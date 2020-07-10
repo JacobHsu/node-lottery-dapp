fe8a6173c02a970e30d0072f80a8991d39540cca
jacobhsu <jacob.hsu.tw@gmail.com>
2020-07-10 14:13:03 +0800

    Updates

---
+++ precache-manifest.c5cad5495ae9ea8a907b09cd428af540.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc887733272002f46d6c3280b25212f6",
+    "url": "/node-lottery-dapp/index.html"
+  },
+  {
+    "revision": "c90e3157d30ae40984d6",
+    "url": "/node-lottery-dapp/static/css/main.751fca17.chunk.css"
+  },
+  {
+    "revision": "235a6a21364df6fbb53b",
+    "url": "/node-lottery-dapp/static/js/2.81df4b54.chunk.js"
+  },
+  {
+    "revision": "71f08bb8ad75542702a8ef6c3e6386cd",
+    "url": "/node-lottery-dapp/static/js/2.81df4b54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c90e3157d30ae40984d6",
+    "url": "/node-lottery-dapp/static/js/main.82bec0c4.chunk.js"
+  },
+  {
+    "revision": "dfbc61a4a610e16e9129",
+    "url": "/node-lottery-dapp/static/js/runtime-main.ab815ecf.js"
+  }
+]);
